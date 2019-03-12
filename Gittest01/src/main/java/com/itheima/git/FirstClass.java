0f91fe53ec96c7b73e3171e675f02a03bb6dc549
admin <admin@163.com>
2019-03-12 11:00:24 +0800

    提交

---
+++ Gittest01/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello Git Test!!1");
+        System.out.println("hello Git Test!!2");
+        System.out.println("hello Git Test!!3");
+        System.out.println("hello Git Test!!4");
+        System.out.println("hello Git Test!!5");
+    }
+}
